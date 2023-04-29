c3b3e388b6e577ea12c8a8cc15bda400af4382a1
yanndepps <yann.deperrot@protonmail.com>
2023-04-29 07:07:37 +0200

    init U3 -> skewing

---
+++ src/U3/sketch_01.js
@@ -0,0 +1,32 @@
+/*
+ * U3 -> sketch_01 : Skewing
+*/
+
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [1080, 1080],
+  animate: true,
+  context: '2d'
+};
+
+const sketch = () => {
+	let x, y, w, h;
+  return ({ context, width, height }) => {
+		context.fillStyle = 'white';
+		context.fillRect(0, 0, width, height);
+
+		x = width * 0.5;
+		y = height * 0.5;
+		w = width * 0.6;
+		h = height * 0.1;
+
+		context.save();
+		context.translate(x, y);
+		context.strokeStyle = 'blue';
+		context.strokeRect(w * -0.5, h * -0.5, w, h);
+		context.restore();
+  };
+};
+
+canvasSketch(sketch, settings);
