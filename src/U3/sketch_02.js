815dd0e6e80fa886de085e485edabb3bdbe291ff
yanndepps <yann.deperrot@protonmail.com>
2023-05-08 10:32:09 +0200

    done sketch_02

--- src/U3/sketch_02.js
+++ src/U3/sketch_02.js
@@ -4,6 +4,7 @@
 */
 
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
 	dimensions: [1080, 1080],
@@ -13,6 +14,7 @@ const settings = {
 
 const sketch = () => {
 	let x, y, w, h;
+	let rx, ry, angle;
 	return ({ context, width, height }) => {
 		context.fillStyle = 'white';
 		context.fillRect(0, 0, width, height);
@@ -24,19 +26,32 @@ const sketch = () => {
 
 		context.save();
 		context.translate(x, y);
-		context.translate(w * -0.5, h * -0.5);
 		context.strokeStyle = 'black';
 
-		// draw the rect point by point
-		context.beginPath();
-		context.moveTo(0, 0);
-		context.lineTo(w, 0);
-		context.lineTo(w, h);
-		context.lineTo(0, h);
-		context.closePath();
-		context.stroke();
+		drawSkewedRect({ context });
+		// context.stroke();
+
 		context.restore();
 	};
 };
+
+const drawSkewedRect = ({ context, w = 600, h = 200, degrees = -45 }) => {
+	const angle = math.degToRad(degrees);
+	const rx = Math.cos(angle) * w;
+	const ry = Math.sin(angle) * w;
+
+	context.save();
+	context.translate(rx * -0.5, (ry + h) * -0.5);
+
+	context.beginPath();
+	context.moveTo(0, 0);
+	context.lineTo(rx, ry);
+	context.lineTo(rx, ry + h);
+	context.lineTo(0, h);
+	context.closePath();
+	context.stroke();
+
+	context.restore();
+}
 
 canvasSketch(sketch, settings);
