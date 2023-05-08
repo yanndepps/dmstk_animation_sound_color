c922a6e11849c1280d8e4f7184b1ae3c701b9ea2
yanndepps <yann.deperrot@protonmail.com>
2023-05-08 13:00:19 +0200

    wip colors

--- src/U3/sketch_04.js
+++ src/U3/sketch_04.js
@@ -6,6 +6,7 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const risoColors = require('riso-colors');
 
 const settings = {
 	dimensions: [1080, 1080],
@@ -14,30 +15,43 @@ const settings = {
 };
 
 const sketch = ({ context, width, height }) => {
-	let x, y, w, h;
+	let x, y, w, h, fill, stroke;
 	const num = 20;
 	const degrees = -30;
 
 	const rects = [];
+	const rectColors = [
+		random.pick(risoColors),
+		random.pick(risoColors),
+		random.pick(risoColors),
+	];
+
 	for (let i = 0; i < num; i++) {
 		x = random.range(0, width);
 		y = random.range(0, height);
 		w = random.range(200, 600);
 		h = random.range(40, 200);
-		rects.push({ x, y, w, h });
+
+		fill = random.pick(rectColors).hex;
+		stroke = random.pick(rectColors).hex;
+
+		rects.push({ x, y, w, h, fill, stroke });
 	}
 
 	return ({ context, width, height }) => {
-		context.fillStyle = 'white';
+		context.fillStyle = fill;
 		context.fillRect(0, 0, width, height);
 
 		rects.forEach(rect => {
-			const { x, y, w, h } = rect;
+			const { x, y, w, h, fill, stroke } = rect;
 			context.save();
 			context.translate(x, y);
-			context.strokeStyle = 'black';
+			context.strokeStyle = stroke;
+			context.fillStyle = fill;
 
 			drawSkewedRect({ context, w, h, degrees });
+			context.stroke();
+			context.fill();
 
 			context.restore();
 		});
@@ -58,7 +72,7 @@ const drawSkewedRect = ({ context, w = 600, h = 200, degrees = -45 }) => {
 	context.lineTo(rx, ry + h);
 	context.lineTo(0, h);
 	context.closePath();
-	context.stroke();
+	// context.stroke();
 
 	context.restore();
 }
