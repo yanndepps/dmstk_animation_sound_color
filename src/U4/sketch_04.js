1d6897642b6b8d76fc89f53b41057f5cb09d1f80
yanndepps <yann.deperrot@protonmail.com>
2023-05-18 12:36:46 +0200

    defined grid structures

--- src/U4/sketch_04.js
+++ src/U4/sketch_04.js
@@ -11,129 +11,56 @@ const settings = {
 	animate: true,
 };
 
-let elCanvas;
-let points;
-
-const sketch = ({ canvas }) => {
-	points = [
-		new Point({ x: 200, y: 540 }),
-		new Point({ x: 400, y: 300 }),
-		new Point({ x: 800, y: 540 }),
-		new Point({ x: 600, y: 700 }),
-		new Point({ x: 640, y: 900 }),
-	];
-
-	canvas.addEventListener('mousedown', onMouseDown);
-	elCanvas = canvas;
+const sketch = ({ width, height }) => {
+	// num cols & rows in grid
+	const cols = 12;
+	const rows = 6;
+	const numCells = cols * rows;
+	// grid width & height
+	const gw = width * 0.8;
+	const gh = height * 0.8;
+	// dim of each cell
+	const cw = gw / cols;
+	const ch = gh / rows;
+	// margins around grid
+	const mx = (width - gw) * 0.5;
+	const my = (height - gh) * 0.5;
+	// array to store points
+	const points = [];
+	// for all grid cells
+	// define x,y & and push new points
+	// into the array
+	let x, y;
+	for (let i = 0; i < numCells; i++) {
+		x = (i % cols) * cw;
+		y = Math.floor(i / cols) * ch;
+		points.push(new Point({ x, y }));
+	}
 
 	return ({ context, width, height }) => {
-		context.fillStyle = 'white';
+		context.fillStyle = 'black';
 		context.fillRect(0, 0, width, height);
-
-		// straights
-		context.fillStyle = '#999';
-		context.beginPath();
-		context.moveTo(points[0].x, points[0].y);
-		for (let i = 1; i < points.length; i++) {
-			context.lineTo(points[i].x, points[i].y);
-		}
-		context.stroke();
-
-		// curves
-		// context.beginPath();
-		// context.moveTo(points[0].x, points[0].y);
-		// for (let i = 1; i < points.length; i += 2) {
-		// 	context.quadraticCurveTo(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
-		// }
-		// context.stroke();
-
-		context.beginPath();
-		for (let i = 0; i < points.length - 1; i++) {
-			const curr = points[i];
-			const next = points[i + 1];
-			const mx = curr.x + (next.x - curr.x) * 0.5;
-			const my = curr.y + (next.y - curr.y) * 0.5;
-
-			// draw mid points
-			// context.beginPath();
-			// context.arc(mx, my, 5, 0, Math.PI * 2);
-			// context.fillStyle = 'blue';
-			// context.fill();
-
-			// draw curves
-			if (i == 0) context.moveTo(curr.x, curr.y);
-			else if (i == points.length - 2) context.quadraticCurveTo(curr.x, curr.y, next.x, next.y);
-			else context.quadraticCurveTo(curr.x, curr.y, mx, my);
-		}
-		context.lineWidth = 4;
-		context.strokeStyle = 'blue';
-		context.stroke();
-
-		points.forEach(point => {
-			point.draw(context);
-		});
-
+		// transformation block
 	};
-};
-
-const onMouseDown = (e) => {
-	window.addEventListener('mousemove', onMouseMove);
-	window.addEventListener('mouseup', onMouseUp);
-
-	const x = Math.round((e.offsetX / elCanvas.offsetWidth) * elCanvas.width);
-	const y = Math.round((e.offsetY / elCanvas.offsetHeight) * elCanvas.height);
-
-	let hit = false;
-	points.forEach(point => {
-		point.isDragging = point.hitTest(x, y);
-		if (!hit && point.isDragging) hit = true;
-	});
-
-	if (!hit) points.push(new Point({ x, y }));
-};
-
-const onMouseMove = (e) => {
-	const x = Math.round((e.offsetX / elCanvas.offsetWidth) * elCanvas.width);
-	const y = Math.round((e.offsetY / elCanvas.offsetHeight) * elCanvas.height);
-
-	points.forEach(point => {
-		if (point.isDragging) {
-			point.x = x;
-			point.y = y;
-		}
-	});
-};
-
-const onMouseUp = () => {
-	window.removeEventListener('mousemove', onMouseMove);
-	window.removeEventListener('mouseup', onMouseUp);
 };
 
 canvasSketch(sketch, settings);
 
 class Point {
-	constructor({ x, y, control = false }) {
+	constructor({ x, y }) {
 		this.x = x;
 		this.y = y;
-		this.control = control;
 	}
 
 	draw(context) {
 		context.save();
 		context.translate(this.x, this.y);
-		context.fillStyle = this.control ? 'red' : 'black';
+		context.fillStyle = 'red';
 
 		context.beginPath();
 		context.arc(0, 0, 10, 0, Math.PI * 2);
 		context.fill();
 
 		context.restore();
-	}
-
-	hitTest(x, y) {
-		const dx = this.x - x;
-		const dy = this.y - y;
-		const dd = Math.sqrt(dx * dx + dy * dy);
-		return dd < 20;
 	}
 }
