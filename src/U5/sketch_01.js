acba83536d667ade4273ca27a35394a2ebadfb0d
yanndepps <yann.deperrot@protonmail.com>
2023-05-19 16:04:52 +0200

    done sketch_01

--- src/U5/sketch_01.js
+++ src/U5/sketch_01.js
@@ -1,6 +1,6 @@
 /*
  * U5 -> sketch_01 : Sound
- * ...
+ * Pressing Play
  */
 
 const canvasSketch = require('canvas-sketch');
@@ -11,11 +11,30 @@ const settings = {
 	animate: false,
 };
 
+let audio;
+
 const sketch = () => {
+	// html audio el
+	// doesn't need to be added to body, only
+	// exist as an entity
+	audio = document.createElement('audio');
+	// path to audio file
+	audio.src = './assets/audio/snd_01.flac';
+
 	return ({ context, width, height }) => {
 		context.fillStyle = 'white';
 		context.fillRect(0, 0, width, height);
 	};
 };
+
+// play/pause audio on mouse release
+const addListeners = () => {
+	window.addEventListener('mouseup', () => {
+		if (audio.paused) audio.play();
+		else audio.pause();
+	});
+};
+
+addListeners();
 
 canvasSketch(sketch, settings);
