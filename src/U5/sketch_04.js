2a71dc4da89f814f50d6ccb68ea3248b00526e4b
yanndepps <yann.deperrot@protonmail.com>
2023-05-21 07:03:41 +0200

    done arcs

--- src/U5/sketch_04.js
+++ src/U5/sketch_04.js
@@ -5,6 +5,7 @@
 
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 const eases = require('eases');
 
 const settings = {
@@ -16,36 +17,67 @@ const settings = {
 let audio;
 let audioContext, audioData, sourceNode, analyserNode;
 let manager;
+let minDb, maxDb;
 
 const sketch = () => {
 	const numCircles = 5;
 	const numSlices = 9;
 	const slice = Math.PI * 2 / numSlices;
 	const radius = 200;
-	const bins = [4, 12, 37];
+
+	const bins = [];
+
+	const lineWidths = [];
+	let lineWidth, bin, mapped;
+
+	// TODO
+	for (let i = 0; i < numCircles * numSlices; i++) {
+		bin = random.rangeFloor(4, 64);
+		if (random.value() > 0.5) bin = 0;
+		bins.push(bin);
+	}
+
+	// eases
+	for (let i = 0; i < numCircles; i++) {
+		const t = i / (numCircles - 1);
+		lineWidth = eases.quadIn(t) * 200 + 20;
+		lineWidths.push(lineWidth);
+	}
 
 	return ({ context, width, height }) => {
 		context.fillStyle = '#EEEAE0';
 		context.fillRect(0, 0, width, height);
 
-		// if (!audioContext) return;
-		// analyserNode.getFloatFrequencyData(audioData);
+		if (!audioContext) return;
+		analyserNode.getFloatFrequencyData(audioData);
 
 		// only translate once
 		context.save();
 		context.translate(width * 0.5, height * 0.5);
+
+		let cradius = radius;
 
 		// loop through both circles & slices
 		for (let i = 0; i < numCircles; i++) {
 			context.save();
 			for (let j = 0; j < numSlices; j++) {
 				context.rotate(slice);
-				context.lineWidth = 10;
+				context.lineWidth = lineWidths[i];
+
+				bin = bins[i * numSlices + j];
+				if (!bin) continue;
+
+				mapped = math.mapRange(audioData[bin], minDb, maxDb, 0, 1, true);
+				lineWidth = lineWidths[i] * mapped;
+				if (lineWidth < 1) continue;
+
+				context.lineWidth = lineWidth;
+
 				context.beginPath();
-				// TODO
-				context.arc(0, 0, radius + i * 50, 0, slice);
+				context.arc(0, 0, cradius + context.lineWidth * 0.5, 0, slice);
 				context.stroke();
 			}
+			cradius += lineWidths[i];
 			context.restore();
 		}
 
@@ -78,6 +110,9 @@ const createAudio = () => {
 	analyserNode.fftSize = 512;
 	analyserNode.smoothingTimeConstant = 0.9;
 	sourceNode.connect(analyserNode);
+
+	minDb = analyserNode.minDecibels;
+	maxDb = analyserNode.maxDecibels;
 
 	// store the audio data into a typed array
 	audioData = new Float32Array(analyserNode.frequencyBinCount);
