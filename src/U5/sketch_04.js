2f97d98915fe339a24d67a194141150a547c23ab
yanndepps <yann.deperrot@protonmail.com>
2023-05-21 06:09:44 +0200

    wip arcs

--- src/U5/sketch_04.js
+++ src/U5/sketch_04.js
@@ -17,38 +17,44 @@ let audioContext, audioData, sourceNode, analyserNode;
 let manager;
 
 const sketch = () => {
+	const numCircles = 5;
+	const numSlices = 9;
+	const slice = Math.PI * 2 / numSlices;
+	const radius = 200;
 	const bins = [4, 12, 37];
+
 	return ({ context, width, height }) => {
-		context.fillStyle = 'white';
+		context.fillStyle = '#EEEAE0';
 		context.fillRect(0, 0, width, height);
 
-		// read the audio data from the analyser node on every frame
-		if (!audioContext) return;
-		analyserNode.getFloatFrequencyData(audioData);
+		// if (!audioContext) return;
+		// analyserNode.getFloatFrequencyData(audioData);
 
-		for (let i = 0; i < bins.length; i++) {
-			const bin = bins[i];
-			const mapped = math.mapRange(audioData[bin], analyserNode.minDecibels, analyserNode.maxDecibels, 0, 1, true);
-			const radius = mapped * 300;
+		// only translate once
+		context.save();
+		context.translate(width * 0.5, height * 0.5);
 
+		// loop through both circles & slices
+		for (let i = 0; i < numCircles; i++) {
 			context.save();
-			context.translate(width * 0.5, height * 0.5);
-			context.lineWidth = 10;
-			context.beginPath();
-			context.arc(0, 0, radius, 0, Math.PI * 2);
-			context.stroke();
+			for (let j = 0; j < numSlices; j++) {
+				context.rotate(slice);
+				context.lineWidth = 10;
+				context.beginPath();
+				// TODO
+				context.arc(0, 0, radius + i * 50, 0, slice);
+				context.stroke();
+			}
 			context.restore();
 		}
 
+		context.restore();
 	};
 };
 
 const addListeners = () => {
 	window.addEventListener('mouseup', () => {
-		// only call if no audio ctx
 		if (!audioContext) createAudio();
-		// play/pause
-		// enable sketch manager
 		if (audio.paused) {
 			audio.play();
 			manager.play();
@@ -65,25 +71,15 @@ const createAudio = () => {
 	audio.src = './assets/audio/snd_01.flac';
 
 	audioContext = new AudioContext();
-	// add input to audio ctx
 	sourceNode = audioContext.createMediaElementSource(audio);
-	// connect to speakers
 	sourceNode.connect(audioContext.destination);
-	// analyser node
 	analyserNode = audioContext.createAnalyser();
-	// fft
 	analyserNode.fftSize = 512;
-	// less jumpy animation
 	analyserNode.smoothingTimeConstant = 0.9;
-	// also connect the src node to the analyser node
 	sourceNode.connect(analyserNode);
 
 	// store the audio data into a typed array
 	audioData = new Float32Array(analyserNode.frequencyBinCount);
-
-	// check the length of audio data
-	// same as frequecyBinCount()
-	// console.log(audioData.length);
 };
 
 // average of the audio data values
@@ -99,9 +95,7 @@ const getAverage = (data) => {
 // async sketch manager
 const start = async () => {
 	addListeners();
-	// store canvasSketch ref to the sketch manager
 	manager = await canvasSketch(sketch, settings);
-	// sketch in a pause state
 	manager.pause();
 };
 
