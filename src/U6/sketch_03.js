f2484bf5770b2e0211922bba6ea18d96362d2cff
yanndepps <yann.deperrot@protonmail.com>
2023-05-22 18:11:08 +0200

    done distribution

--- src/U6/sketch_03.js
+++ src/U6/sketch_03.js
@@ -4,6 +4,8 @@
  */
 
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+const eases = require('eases');
 
 const settings = {
 	dimensions: [1024, 1024],
@@ -16,18 +18,57 @@ const cursor = { x: 9999, y: 9999 };
 let elCanvas;
 
 const sketch = ({ width, height, canvas }) => {
-	let x, y, particle;
-	elCanvas = canvas;
+	let x, y, particle, radius;
+	let pos = [];
 
+	const numCircles = 15;
+	const gapCircle = 8;
+	const gapDot = 4;
+	let dotRadius = 12;
+	let cirRadius = 0;
+	const fitRadius = dotRadius;
+
+	elCanvas = canvas;
 	canvas.addEventListener('mousedown', onMouseDown);
 
-	// populate the array with x num of particles
-	for (let i = 0; i < 1; i++) {
-		x = width * 0.5;
-		y = height * 0.5;
-		particle = new Particle({ x, y });
-		particles.push(particle);
+	for (let i = 0; i < numCircles; i++) {
+		const circumference = Math.PI * 2 * cirRadius;
+		const numFit = i ? Math.floor(circumference / (fitRadius * 2 + gapDot)) : 1;
+		const fitSlice = Math.PI * 2 / numFit;
+
+		for (let j = 0; j < numFit; j++) {
+			// angle for each dot
+			const theta = fitSlice * j;
+			x = Math.cos(theta) * cirRadius;
+			y = Math.sin(theta) * cirRadius;
+
+			// center again
+			x += width * 0.5;
+			y += height * 0.5;
+
+			radius = dotRadius;
+
+			particle = new Particle({ x, y, radius });
+			particles.push(particle);
+		}
+
+		// inc on each iteration
+		cirRadius += fitRadius * 2 + gapCircle;
+		dotRadius = (1 - eases.quadOut(i / numCircles)) * fitRadius;
 	}
+
+	// populate the array with x num of particles
+	// for (let i = 0; i < 200; i++) {
+	// 	x = width * 0.5;
+	// 	y = height * 0.5;
+
+	// 	random.insideCircle(400, pos);
+	// 	x += pos[0];
+	// 	y += pos[1];
+
+	// 	particle = new Particle({ x, y });
+	// 	particles.push(particle);
+	// }
 
 	return ({ context, width, height }) => {
 		context.fillStyle = 'black';
@@ -87,10 +128,10 @@ class Particle {
 		// size
 		this.radius = radius;
 
-		this.minDist = 100;
-		this.pushFactor = 0.02;
-		this.pullFactor = 0.004;
-		this.dampFactor = 0.95;
+		this.minDist = random.range(100, 200);
+		this.pushFactor = random.range(0.01, 0.02);
+		this.pullFactor = random.range(0.002, 0.006);
+		this.dampFactor = random.range(0.90, 0.95);
 	}
 
 	update() {
